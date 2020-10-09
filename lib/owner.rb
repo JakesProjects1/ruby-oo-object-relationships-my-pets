145a97c176a13d23e95ef4950724164c3bf3ebde
jrcliff <justoon7.jc@gmail.com>
2020-10-09 16:14:00 -0400

    justin, jacob, njeri

--- lib/owner.rb
+++ lib/owner.rb
@@ -11,6 +11,7 @@ class Owner
       @name = name
       @species = "human"
       @@all << self
+     
    end
    #binding.pry
 
@@ -34,17 +35,57 @@ class Owner
     Cat.all.select do |cat|
      cat.owner == self
     end
+   
    end
 
    def dogs
     Dog.all.select do |dog|
       dog.owner == self
     end
+ 
    end
 
-  def buy_cat(cat)
-    cat. self
+  def buy_cat(name)
+    Cat.new(name, self)
   end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.map do |dog|
+      dog.mood = "happy"
+      # binding.pry
+    end
+  end
+
+  def feed_cats 
+    self.cats.map do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  
+  def sell_pets
+    self.dogs.map do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+    self.cats.map do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+  end
+
+  def list_pets
+     "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    
+  end
+
+
+
+
    
 
 
